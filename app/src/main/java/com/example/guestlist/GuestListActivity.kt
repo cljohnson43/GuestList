90b35b1a49aed3117361069e4d9455960dc3a296
Chris Johnson <cljohnson4343@gmail.com>
2019-10-09 22:16:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/guestlist/GuestListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.guestlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuestListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guest_list)
+    }
+}
